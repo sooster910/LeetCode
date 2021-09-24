46f469edffbe7716544d17f94d464b3a674fc4fc
Hyunsu Joo <hs9880@gmail.com>
2021-09-24 18:10:47 +0900

    Time: 292 ms (28.39%), Space: 45.2 MB (29.41%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,42 +3,43 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let str= s;
-     const set = new Set();  
-    let max = 0; // max length of substring
-    let length = 0; // length of set
-    let start = 0;
-    
-    for(let end = 0; end<str.length; end++){
-        //iterate하면서 set에 추가한다 
-        //조건1. 만약 추가하려는 문자가 set에 있다면?
-      
-        // 그리고 기존의 set에서 중복되는 문자를 지우면 substring의 기준에 위배 되는가?
-        // 위배된다면, 아예 set을 지우고  새로 시작 
+        if(s.length <=1) return s.length;
+
+    //create set in order to detect repeating
+    const set = new Set();
+
+    // loop from first string
+    let longest = 0;
+   
+
+    for(let left = 0; left<s.length;left++){
+         let right = left;
         
-        if(set.has(str[end])){
-           while(str[start]!==str[end]){
-               set.delete(str[start++]);
-           }
-            set.delete(str[start++]);
-            
+        while(right < s.length){
+            //set에 중복되어 있는지 확인
+            //만약 없다면, 
+            if(!set.has(s[right]) ){
+                     // set에 추가하기
+                     set.add(s[right]);
+                      // 그리고 right ++ 하기 
+             right++;
+            }else{
+                   //만약 있다면, 
+
+            //set.size와 longest와 비교하여서 더 큰수를 longest에 지정하기 
+                longest= set.size >longest ? set.size: longest;
+            //현재 있던 set모두 지우기 
+                set.clear();
+            //현재 observe 하고 있는 요소만 추가하기 
+                // set.add(s[right]);
+            // right를 0으로 리셋하기 
+            // right = 0;
+            // //break하기 
+            break;
+            }
+         
         }
-        
-        
-        //그리고 현재 문자를 set에 추가 해야함 . 
-        set.add(str[end]);
-
-        //set length 구하기
-        length = set.size
-        // set length들 중에 max값 update 하기 
-        max = Math.max(max, length)
-        //조건2. 만약 추가하려는 문자가 set에 있다면?
-        //그리고 기존의 set에서 중복되는 문자를 지우면 substring의 기준에 위배 되는가?
-        //위배가 안된다면, set에서 중복되는 문자를 지운다. 
-
-       
-
     }
-    console.log(max)
-    return max
+
+    return longest;
 };

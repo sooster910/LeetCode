bba394b9ffcc88d54ffbbde194bade06cddf933b
Hyunsu Joo <hs9880@gmail.com>
2021-10-04 18:45:50 +0900

    Time: 135 ms (6.93%), Space: 40.5 MB (23.10%) - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -3,19 +3,14 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let profit =0;
-    for(let i=0;i<prices.length-1; i++){
-     
-        
-            
-        if(prices[i+1] - prices[i] > 0){
-            //buy 
-            profit+= (prices[i+1] - prices[i]) 
-        }else{
-            //sell
-            continue;
-        }
+    let profit = 0;
+  
+    // next주식이 현재 값보다 크면 산다. 
+    for(let i=0;i<prices.length-1;i++){
+        if(prices[i+1]- prices[i] > 0){
+            profit+= (prices[i+1] - prices[i])
+        } 
     }
+    return profit;
     
-    return profit
 };

dbbd52070e25f45a889a858d20360e973c84bae6
Hyunsu Joo <hs9880@gmail.com>
2021-11-30 20:54:33 +0900

    Time: 96 ms (67.33%), Space: 42.5 MB (49.37%) - LeetHub

--- number-of-islands/number-of-islands.js
+++ number-of-islands/number-of-islands.js
@@ -1,39 +1,33 @@
-/**
- * @param {character[][]} grid
- * @return {number}
- */
-var numIslands = function(grid) {
-     let result = 0;
-  const isNotInRange = (x, y) =>
-    Number(x) < 0 ||
-    Number(x) >= grid.length ||
-    Number(y) < 0 ||
-    Number(y) >= grid[0].length;
-
-  const dx = [-1, 0, 1, 0];
-  const dy = [0, 1, 0, -1];
-
-  function dfs(x, y) {
-    if (isNotInRange(x, y)) return;
-     if(grid[x][y]!=="1") return;
-    //방문한 곳은 1이 아닌 수로 바꾸기 ->0
-    grid[x][y] = "0";
-
-    dfs(dx[0] + Number(x), dy[0] + Number(y)); //북
-    dfs(dx[1] + Number(x), dy[1] + Number(y)); //동
-    dfs(dx[2] + Number(x), dy[2] + Number(y)); //남
-    dfs(dx[3] + Number(x), dy[3] + Number(y)); //서z
-
-  }
-
+var numIslands = function (grid) {
+  const visited = grid.map((v) => v.map((v) => false));
+  let result = 0;
   for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j] === "1") {
-        dfs(i, j);
-          result+=1
-      }
+    for (let j = 0; j < grid[i].length; j++) {
+      if(dfs(i, j, grid, visited)){
+        result+=1;
+                      
+      };
     }
   }
-    return result
+
+  return result;
+};
+
+
+const dfs = function (x, y, grid, visited) {
+  x = Number(x);
+  y = Number(y);
     
+
+ if (0 > x || x >= grid.length || 0 > y || y >= grid[0].length) return false;
+    if(!visited[x][y] && (grid[x][y] === "1")){
+          visited[x][y] = true;
+    dfs(x+1,y , grid,visited)
+    dfs(x,y+1,grid,visited)
+    dfs(x-1,y,grid,visited)
+    dfs(x,y-1,grid,visited)
+    return true
+ } 
+return false
+  
 };
